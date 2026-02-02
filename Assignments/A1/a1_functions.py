c964b777ab55ea2cb3109bdce0b6926348ac88d9
mangofish369 <todologd@gmail.com>
2026-02-02 13:08:28 -0500

    part 1 done

--- Assignments/A1/a1_functions.py
+++ Assignments/A1/a1_functions.py
@@ -35,21 +35,52 @@ def load_data(file_path: str) -> pd.DataFrame:
 ### Task 1: Data Cleaning and Preparation ###
 def clean_volume(df: pd.DataFrame) -> None:
     df["Sum of Volume"] = df["Sum of Volume"].astype("int64")
-    df.rename(columns = {"Sum of Volume": VOLUME})
+    df.rename(columns = {"Sum of Volume": VOLUME}, inplace=True)
 
 def clean_region(df: pd.DataFrame) -> None:
-    print("abc")
-    
+    df[REGION] = df[REGION].str.removesuffix(" Region")
+    df[REGION] = df[REGION].str.title()
+
+def clean_port(df: pd.DataFrame) -> None:
+    port_list = df["Port of Entry"].str.split(expand = True)
+    df[PORT_ID] = port_list.iloc[:,0]
+    df[PORT_NAME] = port_list.iloc[:, 2]
+    df.drop(columns = ["Port of Entry"], inplace = True)
+
+def clean_data(df: pd.DataFrame) -> None:
+    pd.to_datetime(df[DATE])
+    clean_region(df)
+    clean_volume(df)
+    clean_port(df)
+
+def fill_missing_volumes(df: pd.DataFrame, method: str) -> None:
+    if method == MEAN:
+        mean = df['Sum of Volume'].mean()
+        df['Sum of Volume'] = df['Sum of Volume'].fillna(mean)
+    elif method == MEDIAN:
+        median = df['Sum of Volume'].median()
+        df['Sum of Volume'] = df['Sum of Volume'].fillna(median)
 
 
 ### Task 2: Data Exploration and Analysis Functions ###
+def filter_with_volume(df: pd.DataFrame, column: str, value: str, volume: int) -> pd.DataFrame:
+    filter = df[(df[column] == value) & (df[VOLUME] >= volume)]
+    return filter
+
 
 
 if __name__ == "__main__":
-    pass 
+    path = r"Assignments\A1\traveller-report-daily.csv"
+    border_df = load_data(path)
+    fill_missing_volumes(border_df,'median')
+    clean_data(border_df)
 
-    # You may call on your functions here to test them.
+    abc = filter_with_volume(border_df,REGION,'Atlantic',1000)
+    print(border_df.head())
+    print(abc)
 
-    border_df = load_data('traveller-report-daily.csv')
-    #clean_data(border_df)
-    #fill_missing_volumes(border_df)
+    # # You may call on your functions here to test them.
+
+    # border_df = load_data('Assignments\A1\traveller-report-daily.csv')
+    # #clean_data(border_df)
+    # #fill_missing_volumes(border_df)
